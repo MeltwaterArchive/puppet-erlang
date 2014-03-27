fd82a2cbc658c769085af7b555386f2daf5854cc
William Leese <william.leese@meltwater.com>
2014-03-27 12:41:55 +0900

    lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 # building erlang from source.
-# 
+#
 # example usage:
 #    include erlang
 #
@@ -7,45 +7,45 @@
 #    class { 'erlang': version => '14B04' }
 #
 
-class erlang ( $version = "15B01" ) {
+class erlang ( $version = '15B01' ) {
 
-    $gpp = $::operatingsystem ? {
-      RedHat => 'gcc-c++',
-      CentOS => 'gcc-c++',
-      Debian => 'g++',
-      Ubuntu => 'g++',
-    }
+  $gpp = $::operatingsystem ? {
+    RedHat => 'gcc-c++',
+    CentOS => 'gcc-c++',
+    Debian => 'g++',
+    Ubuntu => 'g++',
+  }
 
-    $openssl = $::operatingsystem ? {
-      RedHat => 'openssl-devel',
-      CentOS => 'openssl-devel',
-      Ubuntu => 'libssl-dev',
-      Debian => 'libssl-dev',
-    }
+  $openssl = $::operatingsystem ? {
+    RedHat => 'openssl-devel',
+    CentOS => 'openssl-devel',
+    Ubuntu => 'libssl-dev',
+    Debian => 'libssl-dev',
+  }
 
-    if !defined(Package["ncurses-devel"]) { package { "ncurses-devel": ensure => present } }
+  if !defined(Package['ncurses-devel']) { package { 'ncurses-devel': ensure => present } }
 
-    if !defined(Package[$openssl]) { package { $openssl: ensure => present } }
+  if !defined(Package[$openssl]) { package { $openssl: ensure => present } }
 
-    if !defined(Package[$gpp]) { package { $gpp: ensure => present } }
+  if !defined(Package[$gpp]) { package { $gpp: ensure => present } }
 
-    archive { "erlang$version":
-      url => "http://www.erlang.org/download/otp_src_R${version}.tar.gz",
-      checksum => false,
-      target   => "/usr/src",
-      require => Package["ncurses-devel",$openssl, $gpp ]
-    }
+  archive { "erlang${version}":
+    url      => "http://www.erlang.org/download/otp_src_R${version}.tar.gz",
+    checksum => false,
+    target   => '/usr/src',
+    require  => Package['ncurses-devel',$openssl, $gpp ]
+  }
 
-    exec { "configure erlang $version":
-      cwd => "/usr/src/otp_src_R$version",
-      command => "bash -c './configure --without-javac'",
-      require => Archive["erlang$version"]
-    }
+  exec { "configure erlang ${version}":
+    cwd     => "/usr/src/otp_src_R${version}",
+    command => "bash -c './configure --without-javac'",
+    require => Archive["erlang${version}"],
+  }
 
-    exec { "build and install erlang $version":
-      cwd => "/usr/src/otp_src_R$version",
-      command => "bash -c 'make && make install'",
-      require => Exec["configure erlang $version"],
-    }
+  exec { "build and install erlang ${version}":
+    cwd     => "/usr/src/otp_src_R${version}",
+    command => "bash -c 'make && make install'",
+    require => Exec["configure erlang ${version}"],
+  }
 
 }
